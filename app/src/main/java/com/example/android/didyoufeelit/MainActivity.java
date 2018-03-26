61ab4fd583a7ce855a8f3128eaf79d3e0b23b911
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-03-27 00:59:32 +0200

    Initial commit

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -35,10 +36,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        fetchUSGSData task = new fetchUSGSData();
+        task.execute(USGS_REQUEST_URL);
     }
 
     /**
@@ -53,5 +52,27 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    // private inner class of MainActivity to perform the background task of
+    // reading the API
+    private class fetchUSGSData extends AsyncTask<String, Void, Event> {
+        @Override
+        protected Event doInBackground(String... urls) {
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+            Event result = Utils.fetchEarthquakeData(urls[0]); // we only use one URL
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Event event) {
+            if (event == null) {
+                return;
+            }
+            updateUi(event);
+        }
+
     }
 }
